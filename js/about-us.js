ab0aa9a2ded627f130e8162e1d4632bfc0ffbf44
UJJWAL CHAUDHARY <ujjwalanju2001@gmail.com>
2021-01-08 15:40:26 +0530

    little changes

---
+++ js/about-us.js
@@ -0,0 +1,6 @@
+// Particle.js running
+
+particlesJS.load('particles-js', '../../js/lib/particles/particle-about-us.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
